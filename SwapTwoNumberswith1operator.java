3c061941cdef8af440e8d8a26fb7d394d3440f80
Vinayak Kamath <87109462+vinayakkam@users.noreply.github.com>
2026-05-04 11:58:01 +0530

    Add program to swap two numbers using XOR operator

---
+++ SwapTwoNumberswith1operator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Enter the value of a:");
+        a=sc.nextInt();
+        System.out.println("Enter the value of b:");
+        b=sc.nextInt();
+        System.out.println("Before Swapping:");
+        System.out.println("a:"+a);
+        System.out.println("b:"+b);
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println("After Swapping:");
+        System.out.println("a:"+a);
+        System.out.println("b:"+b);
+    }
+}
