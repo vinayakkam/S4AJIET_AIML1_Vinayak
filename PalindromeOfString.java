60c0287273140d721fd9d91bb165c49afd1746ad
Vinayak Kamath <87109462+vinayakkam@users.noreply.github.com>
2026-05-05 14:10:15 +0530

    Add palindrome checking functionality in Java

---
+++ PalindromeOfString.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter the Number:");
+        String str=s.nextLine();
+        String rev ="";
+        int n=str.length();
+        for(int i=n-1;i>-1;i--){
+            rev = rev + str.charAt(i);
+        }
+        if(rev.equals(str)){
+            System.out.println("The given String is a Palindrome");
+        }
+        else{
+            System.out.println("The given String is not a Palindrome");
+        }
+    }
+}
