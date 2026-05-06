b6dc2243dfabab7f0112d0aebf5d4c769b6f37e7
Vinayak Kamath <87109462+vinayakkam@users.noreply.github.com>
2026-05-06 09:13:43 +0530

    Implement bubble sort algorithm for color sorting

---
+++ SortColorInAArray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        int temp;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(nums[j]>nums[j+1]){
+                    temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+        }
+    }
+}
